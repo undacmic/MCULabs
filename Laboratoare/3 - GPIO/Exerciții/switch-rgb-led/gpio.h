2e0033ea6843cce7c4151d0bce8c771ff85374f6
Dragos Ioana <dragosioana20@gmail.com>
2022-11-07 23:03:58 +0200

    Adăugare Laborator 4 - PIT

--- Laboratoare/3 - GPIO/Exerciții/switch-rgb-led/gpio.h
+++
@@ -1,4 +0,0 @@
-#include "MKL25Z4.h"
-void Switch_Init(void);
-void RGBLed_Init(void);
-void PORTA_IRQHandler(void);
--- Laboratoare/3 - GPIO/Exerciții/switch-rgb-led/main.c
+++
@@ -1,9 +0,0 @@
-#include "gpio.h"
-
-int main()
-{
-	Switch_Init();
-	RGBLed_Init();
-	for(;;) {}
-}
-
